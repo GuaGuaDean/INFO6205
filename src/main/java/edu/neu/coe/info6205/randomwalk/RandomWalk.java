53e14070ec2eba4188bfe9ec06a5194a8c494c78
Nan Deng <deng.n@husky.neu.edu>
2017-09-28 12:50:02 +0800

    Revert "Hw1 RandomWalk"
    This reverts commit cc3a7cceb4959b9690167ff5808faefaa7d8d964.

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -4,28 +4,19 @@
 
 package edu.neu.coe.info6205.randomwalk;
 
-import java.util.Random;
-
 public class RandomWalk {
     private int x = 0;
     private int y = 0;
-    private int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-    private Random rand = new Random();
 
     public void move(int dx, int dy) {
-    	x += dx;
-    	y += dy;
+
     }
 
     public void randomWalk(int n) {
-    	for (int i = 0; i < n; i++) {
-    		int[] dir = dirs[rand.nextInt(dirs.length)];
-    		x += dir[0];
-    		y += dir[1];
-    	}
+        // TODO perform n moves
     }
 
     public double distance() {
-        return Math.sqrt(x*x + y*y);
+        return 0.0;  // TODO you need to implement this
     }
 }
